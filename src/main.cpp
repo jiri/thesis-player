242586165118aba7697287071955e0cb58beefb0
Jiří Šebele <sindriava@gmail.com>
2018-04-25 14:06:13 +0200

    Initialize SDL and create a window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 
+#include <SDL2/SDL.h>
+
+#include <Mcu.hpp>
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window* window = SDL_CreateWindow("", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, 0);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    bool done = false;
+    while (!done) {
+        SDL_Event e;
+        while (SDL_PollEvent(&e)) {
+            switch (e.type) {
+                case SDL_QUIT:
+                    done = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /* Render */
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
+        SDL_RenderClear(renderer);
+
+        // ...
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_Quit();
+
     return 0;
 }
